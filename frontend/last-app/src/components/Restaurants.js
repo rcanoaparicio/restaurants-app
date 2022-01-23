56c2325f3752eaf1a73ff4e6e0aa7fb24839fe15
ramon.cano.aparicio <ramon.cano.aparicio@est.fib.upc.edu>
2022-01-23 12:59:34 +0100

    location working

--- frontend/last-app/src/components/Restaurants.js
+++ frontend/last-app/src/components/Restaurants.js
@@ -5,15 +5,23 @@ const Restaurants = (props) => {
     const [restaurants, setRestaurants] = React.useState([]);
 
     React.useEffect(() => {
-        // Check if user has geolocation activated
+        // Check if user has geolocation available
         if (navigator.geolocation) {
-            // Get the list of restaurants ordered by distance from our position
-            navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(position.coords.latitude, position.coords.longitude)
-                fetch(`http://localhost:3001/restaurants?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`)
-                    .then(res => res.json())
-                    .then(data => setRestaurants(data.restaurants))
-            })
+            var wpid = navigator.geolocation.watchPosition(
+                // Can use geolocation
+                (position) => {
+                    console.log(position.coords.latitude, position.coords.longitude)
+                    fetch(`http://localhost:3001/restaurants?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}`)
+                        .then(res => res.json())
+                        .then(data => setRestaurants(data.restaurants))
+                }, 
+                // Can't use geolocation
+                () => {
+                    fetch("http://localhost:3001/restaurants")
+                        .then(res => res.json())
+                        .then(data => setRestaurants(data.restaurants))
+                });
+            
         } 
         else {
             // Get the list of restaurants in any order
